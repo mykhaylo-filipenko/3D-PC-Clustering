be29f16a794ed061b404b040abcb8a438af48654
mykhaylo-filipenko <mishafilipenko2@gmail.com>
2021-02-07 18:20:54 +0200

    Added some logic

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 3D-PC-Clustering
 To run the project try the next command:
-```python main.py <path_to_your_data> <path_where_to_save_data>```
+```python main.py <1 if you need to process data> <path_to_your_data> <path_where_to_save_data>```
 
 ## Data Presentation
 The data should be presented with the following format ```x y x``` to be able to process if with main.py
--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import sys
 
 # This method  will be never used after the data processing
 def processor(file):
+    print("Converting the data...")
     with open(file, 'r+') as file:
         arr = []
         while True:
@@ -14,7 +15,7 @@ def processor(file):
             arr.append(line.replace(' ', ', ').replace('\n', ',\n'))
         file.truncate(0)
         matrix = [x.split() for x in arr]
-        print([x.split() for x in arr])
+
         for arr in matrix:
             for ind in range(len(arr)):
                 if ind == len(arr) - 1:
@@ -24,15 +25,17 @@ def processor(file):
 
 
 def move(file):
-    shutil.move(file, sys.argv[2])  # sys.argv[2] - directory, where to move the data
+    print("Moving file to correct directory...")
+    shutil.copy(file, sys.argv[3])
 
 
 def main():
-    directories = sys.argv[1]  # sys.argv[1] - directory, where to find the data
-    for subdir, dirs, files in os.walk(directories):
-        for file in files:
-            processor(os.path.join(subdir, file))
-            move(os.path.join(subdir, file))
+    if sys.argv[1] == '1':
+        directories = sys.argv[2]
+        for subdir, dirs, files in os.walk(r'{}'.format(directories)):
+            for file in files:
+                processor(os.path.join(subdir, file))
+                move(os.path.join(subdir, file))
 
 
 if __name__ == '__main__':
